ecc0dec05871da059b5c6138f9401020c9fadc2a
Robert Lang <dev@roblang.de>
2016-09-20 11:20:52 +0200

    fixed test

--- test/core/model/lokiModel_test.js
+++ test/core/model/lokiModel_test.js
@@ -4,20 +4,30 @@ let db = require('../../../lib/core/db').initDb();
 let leaf = require('../../../lib/api/common/model/Leaf');
 leaf.init(db);
 let leafModel = leaf.model;
+const testAddress = "node123";
 
 describe("DB querys for leaf (auto-id=address)", function() {
 
+  before(function(){
+    let leaf = leafModel.findByAddress(testAddress);
+    if (leaf){
+      leafModel.delete(leaf);
+    }
+  });
+
+
   let foundLeaf = {};
   let createdLeaf;
+
   it('Create', function() { // (A)
 
     createdLeaf = leafModel.create({
-      "address" : "node123",
+      "address" : testAddress,
       "foo": "bar"
     });
 
     assert.strictEqual(createdLeaf.foo, "bar");
-    assert.strictEqual(createdLeaf.address, "node123");
+    assert.strictEqual(createdLeaf.address, testAddress);
     assert.ok(createdLeaf.id);
   });
 
@@ -27,15 +37,15 @@ describe("DB querys for leaf (auto-id=address)", function() {
 
     assert.ok(foundLeaf);
     assert.strictEqual(foundLeaf.foo, "bar");
-    assert.strictEqual(foundLeaf.address, "node123");
+    assert.strictEqual(foundLeaf.address, testAddress);
   });
 
   it('FindByAddress', function() { // (A)
 
-    foundLeaf = leafModel.findByAddress("node123");
+    foundLeaf = leafModel.findByAddress(testAddress);
 
     assert.strictEqual(foundLeaf.foo, "bar");
-    assert.strictEqual(foundLeaf.address, "node123");
+    assert.strictEqual(foundLeaf.address, testAddress);
   });
 
   it('Update', function() { // (A)
@@ -44,13 +54,13 @@ describe("DB querys for leaf (auto-id=address)", function() {
     let updatedLeaf = leafModel.update(foundLeaf);
 
     assert.strictEqual(updatedLeaf.foo, "foo");
-    assert.strictEqual(updatedLeaf.address, "node123");
+    assert.strictEqual(updatedLeaf.address, testAddress);
   });
 
   it('Delete', function() { // (A)
 
     leafModel.delete(foundLeaf);
-    foundLeaf = leafModel.find("node123")
+    foundLeaf = leafModel.find(testAddress)
 
     assert.strictEqual(foundLeaf, null);
   });
